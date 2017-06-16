e9706d8fa6e8d1462a5216f588ce4978607ef280
matt <mcloke@mac.com>
2017-06-16 11:28:35 +0100

    Initial Commit

---
+++ VisionNoodling/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VisionNoodling
+//
+//  Created by Matt Cloke on 16/06/2017.
+//  Copyright © 2017 Matt Cloke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
